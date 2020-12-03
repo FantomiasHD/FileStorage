5e8823a8a0876e1b3c7d35fc288e540b31b9a942
Simon Seeger <50206768+FantomiasHD@users.noreply.github.com>
2020-12-03 20:20:16 +0100

    Add files via upload

--- Flask/backend.py
+++ Flask/backend.py
@@ -1,20 +1,29 @@
 from flask import Flask
-from flask import render_template
-from flask import send_file, send_from_directory
+from flask import send_from_directory, request, render_template
+from werkzeug.utils import secure_filename
+
+from os import listdir
+from os.path import isfile, join
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "public"
 
-@app.route('/')
-def hello_world():
-    return render_template("test_temp.html")
+@app.route("/")
+def main():
+    return '<a href="downloader">Test</a>'
 
-@app.route('/download')
-def download():
-    path = "./public/downloadable.txt"
-    return send_file(path, as_attachment=True)
+@app.route('/downloader')
+def downloader():
+    onlyfiles = [f for f in listdir(app.config["UPLOAD_FOLDER"]) if isfile(join(app.config["UPLOAD_FOLDER"], f))]
+    return render_template("test_temp.html", files=onlyfiles)
 
-@app.route('/send_dir/<path:filename>')
-def download_from_dir(filename):
+@app.route('/download/<path:filename>')
+def download(filename):
     return send_from_directory(app.config["UPLOAD_FOLDER"], filename, as_attachment=True)
-    
+
+@app.route('/upload', methods=['GET', 'POST'])
+def uploader():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(app.config["UPLOAD_FOLDER"] + '/' + secure_filename(f.filename))
+    return render_template("uploader.html")
