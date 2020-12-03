c83c2aa23445c1ca6f2b623d6370066065d5f930
Simon Seeger <50206768+FantomiasHD@users.noreply.github.com>
2020-12-03 21:45:20 +0100

    Added Login
    Added Login and bad cookie

--- Flask/backend.py
+++ Flask/backend.py
@@ -1,29 +1,50 @@
 from flask import Flask
-from flask import send_from_directory, request, render_template
+from flask import send_from_directory, request, render_template, make_response, redirect
 from werkzeug.utils import secure_filename
 
 from os import listdir
 from os.path import isfile, join
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "KHC2zY6tCH3ppS4BapA4bWFTrykm0eSJ"
 app.config["UPLOAD_FOLDER"] = "public"
 
-@app.route("/")
+@app.route("/",  methods = ['POST', 'GET'])
 def main():
-    return '<a href="downloader">Test</a>'
+    if request.cookies.get('auth') == 'TGVvSXN0RWluRHJlY2tzc2Fja1VuZEthbm5IaWVyTmljaHRSZWlu':
+        return redirect('/downloader')
+    else:
+        return render_template("Login.html")
+
+
+@app.route('/setcookie', methods=['POST', 'GET'])
+def setcookie():
+    if request.method == 'POST':
+        if request.form['user'] == 'admin':
+            if request.form['pass'] == 'FlaskRulz':
+                resp = redirect('/downloader')
+                resp.set_cookie('auth', 'TGVvSXN0RWluRHJlY2tzc2Fja1VuZEthbm5IaWVyTmljaHRSZWlu')
+                return resp
+    return redirect('/')
 
 @app.route('/downloader')
 def downloader():
-    onlyfiles = [f for f in listdir(app.config["UPLOAD_FOLDER"]) if isfile(join(app.config["UPLOAD_FOLDER"], f))]
-    return render_template("test_temp.html", files=onlyfiles)
+    if request.cookies.get('auth') == 'TGVvSXN0RWluRHJlY2tzc2Fja1VuZEthbm5IaWVyTmljaHRSZWlu':
+        onlyfiles = [f for f in listdir(app.config["UPLOAD_FOLDER"]) if isfile(join(app.config["UPLOAD_FOLDER"], f))]
+        return render_template("test_temp.html", files=onlyfiles)
+    return redirect('/', 403)
 
 @app.route('/download/<path:filename>')
 def download(filename):
-    return send_from_directory(app.config["UPLOAD_FOLDER"], filename, as_attachment=True)
+    if request.cookies.get('auth') == 'TGVvSXN0RWluRHJlY2tzc2Fja1VuZEthbm5IaWVyTmljaHRSZWlu':
+        return send_from_directory(app.config["UPLOAD_FOLDER"], filename, as_attachment=True)
+    return redirect('/', 403)
 
 @app.route('/upload', methods=['GET', 'POST'])
 def uploader():
-    if request.method == 'POST':
-        f = request.files['file']
-        f.save(app.config["UPLOAD_FOLDER"] + '/' + secure_filename(f.filename))
-    return render_template("uploader.html")
+    if request.cookies.get('auth') == 'TGVvSXN0RWluRHJlY2tzc2Fja1VuZEthbm5IaWVyTmljaHRSZWlu':
+        if request.method == 'POST':
+            f = request.files['file']
+            f.save(app.config["UPLOAD_FOLDER"] + '/' + secure_filename(f.filename))
+        return render_template("uploader.html")
+    return redirect('/', 403)
